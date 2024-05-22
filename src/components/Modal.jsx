02665cd7dc793ec52cb2be6f1b6cd37fc4162caa
Guravaiah Nallabothula <n180027@rguktn.ac.in>
2024-05-22 19:35:57 +0530

    Initial commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import {  useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+function Modal({ open,children }) {
+  const dialog = useRef();
+  useEffect(()=>{
+    if(open){
+      dialog.current.showModal();
+    }else{
+      dialog.current.close();
+    }
+  },[open]);
+  // useImperativeHandle(ref, () => {
+  //   return {
+  //     open: () => {
+  //       dialog.current.showModal();
+  //     },
+  //     close: () => {
+  //       dialog.current.close();
+  //     },
+  //   };
+  // });
+
+  return createPortal(
+    <dialog className="modal" ref={dialog} >
+      {open ? children : null}
+    </dialog>,
+    document.getElementById('modal')
+  );
+};
+
+export default Modal;
---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useEffect,useState } from 'react';
+const TIMER=3000;
+export default function ProgressBar() {
+    const [remainingTime,setRemainingTime]=useState(TIMER);
+    useEffect(()=>{
+        const interval=setInterval(()=>{
+          setRemainingTime(prevTime =>prevTime-10)
+        },10);
+      
+        return()=>{
+          clearTimeout(interval);
+        }
+      });
+  return (
+    <progress value={remainingTime} max={TIMER}/>
+  )
+}
